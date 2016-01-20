cf5fa5a946bba40eefa037497ee9559f2a578a17
James Chapman <james.chapman@strath.ac.uk>
2016-01-20 16:45:21 +0000

    starting to write a deBruijnifier

---
+++ DeBruijnify.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE GADTs, DataKinds, KindSignatures, StandaloneDeriving #-}
+module DeBrujnify where
+
+import Utils
+import Data.List
+import Data.Maybe
+
+data Raw = RLam String Raw
+         | RApp Raw    Raw
+         | RVar String Nat
+         deriving Show
+
+data Tm (n :: Nat) :: * where
+  Lam :: Tm (Suc n)         -> Tm n
+  App :: Tm n       -> Tm n -> Tm n
+  V   :: Fin n              -> Tm n
+  P   :: String             -> 
+
+deriving instance Show (Tm n)
+
+type SC = Maybe
+
+deBruijnify :: Vec String n -> Raw -> SC (Tm n)
+deBruijnify g (RLam x t) = Lam <$> deBruijnify (VCons x g) t
+deBruijnify g (RApp t u) = App <$> deBruijnify g t <*> deBruijnify g u
+deBruijnify g (RVar x n) = V   <$> velemIndex' x n g
+
--- Utils.hs
+++ Utils.hs
@@ -2,7 +2,7 @@
              DeriveFunctor, StandaloneDeriving  #-}
 module Utils where
 
-data Nat = Zero | Suc Nat
+data Nat = Zero | Suc Nat deriving Show
 
 type One = Suc Zero
 
@@ -15,6 +15,32 @@ deriving instance Show (Fin n)
 
 absurd :: Fin Zero -> a
 absurd k = case k of {}
+
+data Vec x (n :: Nat) where
+  VNil  :: Vec x Zero
+  VCons :: x -> Vec x n -> Vec x (Suc n)
+
+vlookup :: Fin n -> Vec x n -> x
+vlookup FZero    (VCons x _ ) = x
+vlookup (FSuc i) (VCons _ xs) = vlookup i xs
+
+velemIndex :: Eq x => x -> Vec x n -> Maybe (Fin n)
+velemIndex x VNil          = Nothing
+velemIndex x (VCons x' xs) =
+  if x == x' then
+    Just FZero              
+  else
+    fmap FSuc (velemIndex x xs)
+
+velemIndex' :: Eq x => x -> Nat ->  Vec x n -> Maybe (Fin n)
+velemIndex' x n VNil          = Nothing
+velemIndex' x n (VCons x' xs) =
+  if x == x' then
+    case n of
+      Zero  -> Just FZero
+      Suc n -> fmap FSuc (velemIndex' x n xs)
+  else
+    fmap FSuc (velemIndex' x n xs)
 
 -- utilities
 data Bwd x = B0 | Bwd x :< x deriving Functor
