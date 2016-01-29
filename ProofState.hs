32c1006288f3a12d5c7d93f1dda1fc082cd86ff2
James Chapman <james.chapman@strath.ac.uk>
2016-01-29 16:39:15 +0000

    slightly cleaned up resolver

--- ProofState.hs
+++ ProofState.hs
@@ -28,7 +28,7 @@ data Resolution (w :: World) where
   RParam :: Ref w -> Resolution w
   RGlob  :: Global n -> Env (Syn Zero) m w -> Resolution w
 
-instance Weakenable Resolution 
+instance Weakenable Resolution
 
 stripParams :: PZ True v w -> Ex2 (Env (Syn Zero)) w
 stripParams = stripParams' id where
@@ -39,32 +39,50 @@ stripParams = stripParams' id where
   stripParams' w (pz :<: Module _ _ _) = stripParams' w pz
   stripParams' w (pz :<: Middle _ _)   = stripParams' w pz
 
-resolve :: RawLongName -> PZ True v w -> TC Resolution w
-resolve ((x,i),nsteps) = help i 
-  where 
-  help :: Int -> PZ True v w -> TC Resolution w
-  help i L0                                        = No
-  help i (pz :<: Param  y e)                       = case (x == y, i, nsteps) of
-    (True, 0, []) -> Yes $ RParam (extrRef e)
-    (True, 0, _ ) -> No -- looking for module components inside a param
-    (True, i, _ ) -> extwk e $ help (i-1) pz
-    (False, _, _) -> extwk e $ help i pz
-  help i (pz :<: Module y mglob (Wit (pz' :&: _))) = case (x == y, i) of
-    (True,  0) -> case (help2 nsteps mglob pz', stripParams pz) of
-      (Just (Wit glob), Wit2 g) -> Yes (RGlob glob g)
-      _                         -> No
-    (True,  i) -> help (i-1) pz
-    (False, _) -> help i pz
-  help i (pz :<: Middle _ _  )                     = help i pz
 
-  help2 :: [NameStep] -> Maybe (Ex Global) -> PZ False v w -> Maybe (Ex Global)
-  help2 []       mglob pz = mglob
-  help2 ((x,i) : xs) _ pz = help2' i pz
+eqNameStep :: Naming -> NameStep -> Either NameStep ()
+eqNameStep y xi@(x,i) = case (x == y, i) of
+   (True,  0) -> Right ()
+   (True,  i) -> Left (x,i-1)
+   (False, _) -> Left xi 
+
+resolve :: RawLongName -> PZ True v w -> TC Resolution w
+resolve (xi,nsteps) = lookOut xi
+  where 
+  lookOut :: NameStep -> PZ True v w -> TC Resolution w
+  lookOut xi L0                                        = No
+  lookOut xi (pz :<: Param  y e)                       =
+    case eqNameStep y xi of
+       -- found it!
+      Right _ | null nsteps -> Yes $ RParam (extrRef e)
+              -- looking for module components inside a param
+              | otherwise   -> No
+       -- carry on looking
+      Left xi -> extwk e $ lookOut xi pz
+  lookOut xi (pz :<: Module y mglob (Wit (pz' :&: _))) =
+    case eqNameStep y xi of
+      -- found the 'corner'; look inside
+      Right _ -> case (lookInside nsteps mglob pz', stripParams pz) of 
+        (Just (Wit glob), Wit2 g) -> Yes (RGlob glob g)
+        _                         -> No
+      -- carry on looking
+      Left xi -> lookOut xi pz
+  lookOut i (pz :<: Middle _ _  )                     = lookOut i pz
+
+  lookInside :: [NameStep] -> Maybe (Ex Global) -> PZ False v w ->
+                Maybe (Ex Global)
+  lookInside []           mglob pz = mglob -- found it!
+  lookInside (xi : xs) _     pz = lookInside' xi pz
     where
-    help2' :: Int -> PZ False v w -> Maybe (Ex Global)
-    help2' i (pz :<: Param y e) = help2' i pz
-    help2' i (pz :<: Module y mglob (Wit (pz' :&: _))) = case (x == y, i) of
-      (True,  0) -> help2 xs mglob pz'
-      (True,  i) -> help2' (i-1) pz
-      (False, _) -> help2' i pz
+    lookInside' :: NameStep -> PZ False v w -> Maybe (Ex Global)
+    lookInside' _ L0 = Nothing
+    lookInside' xi (pz :<: Param _ _) = lookInside' xi pz
+      -- parameters are not in scope
+    lookInside' xi (pz :<: Module y mglob (Wit (pz' :&: _))) =
+      case eqNameStep y xi of
+        -- found the next step
+        Right _ -> lookInside xs mglob pz'
+        -- carry on looking
+        Left xi -> lookInside' xi pz
+
     
