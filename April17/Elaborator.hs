e89e817e33057ee068d6564183067da52e071268
Conor McBride <conor@strictlypositive.org>
2017-04-04 11:06:49 +0100

    radical surgery (not really)

--- April17/Elaborator.hs
+++ April17/Elaborator.hs
@@ -18,16 +18,16 @@ chkBind gamma s _S _T t = abstract "" <$> chk (gamma :\ (s , "" , _S)) (dive _T)
 chk :: Sorted gamma =>
        Context gamma -> Term Chk ^ gamma -> Raw -> Maybe (Term Chk ^ gamma)
 chk gamma (Star Void :^ r) (RA "Type") = return $ Star Void :^ r 
-chk gamma star@(Star Void :^ r) (RC (RA "Pi") (_S :-: Only _T)) = do
+chk gamma (Star Void :^ r) (RC (RA "Pi") (_S :-: Only _T)) = do
   _S <- chk gamma star _S
-  _T <- chkBind gamma Syny _S (K (Star Void) :^ r) _T
+  _T <- chkBind gamma Syny _S (mapIx K star) _T
   return $ mapIx Pi (pair _S _T )
-chk gamma (Pi _ST :^ r) (RC (RA "\\") (Only t)) = _ST :^ r >^< \_S _T -> do
+chk gamma (Pi _ST :^ r) (RC (RA "\\") (Only t)) = _ST :^ r >^< \ _S _T -> do
   t <- chkBind gamma Syny _S _T t
   return $ mapIx Lam t
 chk gamma _T t = do
-  (s , _S) <- syn gamma t
-  defEq gamma (Star Void :^ oN) _S _T
+  (s ::: _S) <- syn gamma t
+  defEq gamma star _S _T
   return s
 
 fetch :: Context gamma -> Sorty s -> String ->
@@ -43,27 +43,24 @@ fetch (gamma :\ (s',y,i :^ r)) s x =
 
 syn :: Sorted gamma =>
        Context gamma -> Raw ->
-       Maybe (Term Chk ^ gamma , Term Chk ^ gamma) -- thing followed by its type 
+       Maybe (Radical Syn gamma)
 syn gamma (RA x) = do
   (x , _S) <- fetch gamma Syny x
-  return (mapIx (E . V) x, _S)
+  return (mapIx (E . V) x ::: _S)
 syn gamma (RC (RA "the") (_T :-: Only t)) = do
-  _T <- chk gamma (Star Void :^ oN) _T
+  _T <- chk gamma star _T
   t <- chk gamma _T t
-  return (t , _T)
+  return (t ::: _T)
 syn gamma (RC f as) = do
   f <- syn gamma f
   spine gamma f as
 syn _ _ = fail "raised eyebrow"
 
-spine :: Sorted gamma => Context gamma -> (Term Chk ^ gamma, Term Chk ^ gamma) ->
-         NEL Raw -> Maybe (Term Chk ^ gamma , Term Chk ^ gamma)
-spine gamma (f , Pi _ST :^ r) (a :- as) = _ST :^ r >^< \_S _T -> do
-  a <- chk gamma _S a
-  let faT' = (app (pair f a), instantiate _T a)
-  case as of
-    Nothing -> return faT'
-    Just as -> spine gamma faT' as
+spine :: Sorted gamma => Context gamma -> Radical Syn gamma ->
+         NEL Raw -> Maybe (Radical Syn gamma)
+spine gamma h@(f ::: Pi _ST :^ r) (s :- as) = _ST :^ r >^< \ _S _T -> do
+  s <- chk gamma _S s
+  return (app h s)
 spine gamma _ _ = fail "raised eyebrow"
          
 -- render N0 <$> (chk C0 (Star Void :^ OZ) =<< rawString "Pi Type X. Pi X x. X")
--- April17/Kernel.hs
+++ April17/Kernel.hs
@@ -9,19 +9,12 @@ import Utils
 import OPE
 import Data.Void
 
+------------------------------------------------------------------------------
+-- Syntactic Sorts
+------------------------------------------------------------------------------
+
 data Sort = Chk | Syn | Pnt
 
-data Sorty :: Sort -> * where
-  Chky :: Sorty Chk
-  Syny :: Sorty Syn
-  Pnty :: Sorty Pnt
-
-sortEq :: Sorty s -> Sorty s' -> Maybe (s == s')
-sortEq Chky Chky = Just Refl
-sortEq Syny Syny = Just Refl
-sortEq Pnty Pnty = Just Refl
-sortEq _    _    = Nothing
-  
 {-:: * where
   Chk :: Sort
   Syn :: Sort
@@ -34,7 +27,11 @@ sortEq _    _    = Nothing
 -}
 type Sorts = Bwd Sort
 
--- these are normal forms
+
+------------------------------------------------------------------------------
+-- Terms (in normal form)
+------------------------------------------------------------------------------
+
 data Term :: Sort -> (Bwd Sort -> *) where
   Star :: Unit gamma -> Term Chk gamma
   Pi :: (Term Chk  ><  Syn !- Term Chk) gamma -> Term Chk gamma
@@ -43,55 +40,90 @@ data Term :: Sort -> (Bwd Sort -> *) where
   V :: This s gamma -> Term s gamma
   App :: (Term Syn >< Term Chk) gamma -> Term Syn gamma
 
-class Sub (f :: Bwd Sort -> *) where
-  type SubImage f :: Bwd Sort -> *
-  image :: f -:> SubImage f
-  sub :: f gamma' -> Select gamma' s ^ delta ->
-         Instantiate s ^ delta -> SubImage f ^ delta
 
+------------------------------------------------------------------------------
+-- derived constructors for thinned terms
+------------------------------------------------------------------------------
+
+star :: Sorted gamma => Term Chk ^ gamma
+star = Star Void :^ oN
+
+
+------------------------------------------------------------------------------
+-- hereditary substitution
+------------------------------------------------------------------------------
+
+class Sub (f :: Bwd Sort -> *) where
+  sub :: f gamma' -> Select gamma' s ^ delta -> Radical s delta -> f ^ delta
+
+data Radical :: Sort -> Bwd Sort -> * where
+  (:::) :: Term Chk ^ delta -> Term Chk ^ delta -> Radical Syn delta
+infixr 3 :::
+
+-- We substitute a radical for a variable. Radicals can cause computation.
+
+instance Sub (Term Chk) where
+  sub (Pi _ST) xr s = mapIx Pi $ sub _ST xr s
+  sub (Lam t)  xr s = mapIx Lam $ sub t xr s
+  sub (E e)    xr s = stop $ subSyn e xr s
+
+-- substitution in a Term Syn can radicalise it
+subSyn :: Term Syn gamma' ->
+          Select gamma' s ^ delta -> Radical s delta ->
+          Either (Radical Syn delta) (Term Syn ^ delta)
+subSyn (V It) (Top :^ r) s = Left s
+subSyn (App (Pair c f a)) (z :^ r) s = case hits c z of
+    HRight  y c -> Right . mapIx App $  -- right hit means no radical
+      pair (f :^ r -<=- lCoP c) (sub a (y :^ r -<=- rCoP c) s)
+      -- left hit means risk of radical
+    HLeft x   c -> go (subSyn f (x :^ r -<=- lCoP c) s) (a :^ r -<=- rCoP c)
+    HBoth x y c -> go (subSyn f (x :^ r -<=- lCoP c) s)
+                      (sub a (y :^ r -<=- rCoP c) s)
+  where
+    go (Left r)  a = Left (app r a)
+    go (Right e) a = Right (mapIx App (pair e a))
+
+-- we can always stop if a Term Chk is wanted, stripping the spent type
+stop :: Either (Radical Syn delta) (Term Syn ^ delta) -> Term Chk ^ delta
+stop (Left (t ::: _))  = t
+stop (Right e)         = mapIx E e
+
+-- structural rule for pairing
 instance (Sub f , Sub g) => Sub (f >< g) where
-  type SubImage (f >< g) = SubImage f >< SubImage g
-  image (Pair c f g) = Pair c (image f) (image g)
   sub (Pair c f g) (z :^ r) s = case hits c z of
-    HLeft  x c -> pair (sub f (x :^ r -<=- lCoP c) s) (image g :^ r -<=- rCoP c)
-    HRight y c -> pair (image f :^ r -<=- lCoP c) (sub g (y :^ r -<=- rCoP c) s)
+    HLeft  x c -> pair (sub f (x :^ r -<=- lCoP c) s) (g :^ r -<=- rCoP c)
+    HRight y c -> pair (f :^ r -<=- lCoP c) (sub g (y :^ r -<=- rCoP c) s)
     HBoth  x y c ->
       pair (sub f (x :^ r -<=- lCoP c) s) (sub g (y :^ r -<=- rCoP c) s)
 
+-- structural rule for binding
 instance Sub f => Sub (s !- f) where
-  type SubImage (s !- f) = s !- SubImage f
-  image (K f) = K (image f)
-  image (L x f) = L x (image f)
   sub (K f) xr s = mapIx K $ sub f xr s
-  sub (L y f) (x :^ r) (t :^ r') = abstract y (sub f (Pop x :^ OS r) (t :^ O' r'))
+  sub (L y f) (x :^ r) s = abstract y (sub f (Pop x :^ OS r) (radW s)) where
+    radW :: Radical t gamma -> Radical t (gamma :< s)
+    radW (t :^ r ::: _T :^ _R) = t :^ O' r ::: _T :^ O' _R
 
-instance Sub (Term Chk) where
-  type SubImage (Term Chk) = Term Chk
-  image = id
-  sub (Pi _ST) xr s = mapIx Pi $ sub _ST xr s
-  sub (Lam t) xr s = mapIx Lam $ sub t xr s
-  sub (E e) xr s = sub e xr s
 
-instance Sub (Term Syn) where
-  type SubImage (Term Syn) = Term Chk
-  image = E
-  sub (V It) (Top :^ r) s = s
-  sub (App fa) xr s = app (sub fa xr s)
+------------------------------------------------------------------------------
+-- computation is the elimination of radicals
+------------------------------------------------------------------------------
 
-app :: (Term Chk >< Term Chk) ^ delta -> Term Chk ^ delta
-app (Pair c (E e) a :^ r) = E (App (Pair c e a)) :^ r
-app (Pair c (Lam t) a :^ r) =
-  instantiate (t :^ r -<=- lCoP c) (a :^ r -<=- rCoP c) 
-
-type family Instantiate (s :: Sort) :: Bwd Sort -> * where
-  Instantiate Syn = Term Chk
-  --Instantiate Chk = Term Chk -- ain't never gonna happen
-  Instantiate Pnt = Term Pnt
-
-instantiate :: (s !- Term Chk) ^ delta -> Instantiate s ^ delta ->
+app :: Radical Syn delta -> Term Chk ^ delta -> Radical Syn delta
+app (f :^ r ::: Pi _ST :^ _R) s = _ST :^ _R >^< \ _S _T ->
+  (case f of
+    E e   -> mapIx (E . App) (pair (e :^ r) s)
+    Lam t -> instantiate (t :^ r) (s ::: _S)
+  ) ::: instantiate _T (s ::: _S)
+  
+instantiate :: (s !- Term Chk) ^ delta -> Radical s delta ->
                Term Chk ^ delta
 instantiate (K t :^ r) _ = t :^ r
 instantiate (L _ t :^ r) a = sub t (Top :^ r) a
+
+
+------------------------------------------------------------------------------
+-- environments of the unremarkable kind
+------------------------------------------------------------------------------
 
 data Env :: Bwd Sort -> * -> * where
   N0 :: Env B0 a
@@ -100,3 +132,20 @@ data Env :: Bwd Sort -> * -> * where
 deriving instance Foldable (Env gamma)
 deriving instance Functor (Env gamma)
 deriving instance Traversable (Env gamma)
+
+
+------------------------------------------------------------------------------
+-- Singletons for Sorts
+------------------------------------------------------------------------------
+
+data Sorty :: Sort -> * where
+  Chky :: Sorty Chk
+  Syny :: Sorty Syn
+  Pnty :: Sorty Pnt
+
+sortEq :: Sorty s -> Sorty s' -> Maybe (s == s')
+sortEq Chky Chky = Just Refl
+sortEq Syny Syny = Just Refl
+sortEq Pnty Pnty = Just Refl
+sortEq _    _    = Nothing
+  
