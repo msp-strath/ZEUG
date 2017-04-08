5ab9c6f9c3a1c644f8f21bec588c358d69d728a3
Conor McBride <conor@strictlypositive.org>
2017-04-08 17:48:40 +0100

    now you can navigate within the viewport by using   prev   next

--- April17/Command.hs
+++ April17/Command.hs
@@ -9,11 +9,15 @@ import ProofState
 import Elaborator
 
 command :: ProofState -> Raw c -> ([String], Maybe ProofState)
+
+-- MAKING A DEFINITION
 command ps (RC (RA _ x) (Only b)) | isDefBody b = case newName ps x of
   Nothing -> (["I don't like the name " ++ x], Nothing)
   Just x -> case tryElab (ps, C0, x) (define b) of
     Just (_, ps)  -> (["Aye."], Just ps)
     Nothing       -> (["Naw."], Nothing)
+
+-- GIVING A REFINEMENT
 command (Cur ez u (EHole m@(Meta s x _Theta _I) : es))
   (RC (RA _ "=") (Only b)) = case s of
   Pnty -> (["Try points another day."], Nothing)
@@ -23,13 +27,29 @@ command (Cur ez u (EHole m@(Meta s x _Theta _I) : es))
       ( ["Aye."]
       , Just (fwdToGoal $ Cur ez u (updates [m :=> Solve (mapIx IS t)] es))
       )
+
+-- ZOOMING TO A NAME
 command ps@(Cur ez u es) (RC (RA _ "/") (Only (RA _ x))) =
   case fwdToView (Cur B0 (fst (parseName ps x), (Nothing, Nothing))
                       (ez <>> es)) of
     Cur _ (LongName (_ : _), _) [] -> (["Where's " ++ x ++ "?"], Nothing)
     ps -> ([], Just (fwdToGoal ps))
+
+-- ZOOMING OUT
 command (Cur ez u@(LongName p@(_ : _), _) es) (RA _ "<") =
   ([], Just (Cur ez (LongName (init p), (Nothing, Nothing)) es))
+
+-- DOWN ONE
+command (Cur ez u@(p, (_, n)) (e : es)) (RA _ "next")
+  | Just _ <- inView (p, n) e = ([], Just (Cur (ez :< e) u es))
+  | otherwise                 = (["Bump!"], Nothing)
+
+-- UP ONE
+command (Cur (ez :< e) u@(p, (n, _)) es) (RA _ "prev")
+  | Just _ <- inView (p, n) e = ([], Just (Cur ez u (e : es)))
+  | otherwise                 = (["Bump!"], Nothing)
+
+-- BARF!
 command ps c = (["Try doing something else."], Nothing)
 
 isDefBody :: Raw c -> Bool
